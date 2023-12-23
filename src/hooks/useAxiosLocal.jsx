eb795c40e25ed38ef9c95436c955830282a0954d
Rakib mia <rbepari404@gmail.com>
2023-12-23 05:37:38 -0600

    update base url

--- src/hooks/useAxiosLocal.jsx
+++ src/hooks/useAxiosLocal.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosLocal = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://depop-alpha.vercel.app",
 });
 
 const useAxiosLocal = () => {
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosSecure = axios.create({
-    baseURL: "http://localhost:3001"
+    baseURL: "https://depop-alpha.vercel.app"
 })
 
 const useAxiosSecure = () => {
